d8d4a1a725ae2a48c1d54ea2dce888cbe07ec3d6
Kyle-Ruan <kyleryk123@gmail.com>
2018-04-28 08:24:39 +1000

    update

--- lunch-picker-server/src/api/yelp/reviews.js
+++ lunch-picker-server/src/api/yelp/reviews.js
@@ -9,18 +9,17 @@ export const handler = async (event, context, callback) => {
   const { id } = event.pathParameters;
 
   const repository = new RestaurantRepository();
-  const item = await repository.get(id);
+  const item = await repository.getOnlineDetail(id);
 
   if (item && item.reviews) {
     return callback(null, success(item.reviews));
   }
   const apiKey = await getParam('YELP_API_KEY');
   const yelpClient = new YelpClient(apiKey);
-
-  const result = await yelpClient.getReviews(id);
+  const reviews = await yelpClient.getReviews(id);
   try {
-    await repository.saveOnlineReviews(id, 'reviews', result);
-    callback(null, success(result));
+    await repository.saveOnlineReviews(id, 'reviews', reviews);
+    callback(null, success(reviews));
   } catch (error) {
     callback(null, failure(error));
   }
